aecb4d0d5efd20a68ef9f395c40eafc0bbef32bb
Victor Miguel <victorgabrielam12@gmail.com>
2025-11-05 16:25:35 -0300

    Finished

---
+++ src/main/java/br/com/fiap/fintech/config/WebConfig.java
@@ -0,0 +1,18 @@
+package br.com.fiap.fintech.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Permite CORS para todos os endpoints /api
+                .allowedOrigins("http://localhost:5173") // Permite requisições do seu frontend
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
